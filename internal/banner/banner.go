f852f399bc3bbaebfb6b54264e1058fbe20c4204
Wang ZY <76583642+xmx@users.noreply.github.com>
2022-07-25 20:11:57 +0800

    美化 banner

--- internal/banner/banner.go
+++ internal/banner/banner.go
@@ -6,20 +6,6 @@ import (
 	"runtime"
 )
 
-const logo = "\n\u001B[1;32m" +
-	"███████   ██████    ██████\n" +
-	"██       ██    ██  ██     \n" +
-	"███████  ██    ██  ██     \n" +
-	"     ██  ██    ██  ██     \n" +
-	"███████   ██████    ██████\u001B[0m  \u001B[1;35mMINION\u001B[0m\n" +
-	"Powered By: https://security.eastmoney.com\n\n" +
-	"\t进程 PID: \u001B[1;1m%d\u001B[0m\n" +
-	"\t操作系统: \u001B[1;1m%s\u001B[0m\n" +
-	"\t系统架构: \u001B[1;1m%s\u001B[0m\n" +
-	"\t软件版本: \u001B[1;1m%s\u001B[0m\n" +
-	"\t编译时间: \u001B[1;1m%s\u001B[0m\n" +
-	"\t版本控制: \u001B[1;1m%s\u001B[0m\n\n\n"
-
 var (
 	// BuildAt 编译时间, 遵循 time.RFC3339 格式
 	// 转为 go 语言的 time.Time 示例:
